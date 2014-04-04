222a10e8adbdd996f3f0f57f02f17b9c120b762b
huseni <huseni@hotmail.com>
2014-04-04 00:05:19 -0700

    Update follows.js

--- follows.js
+++ follows.js
@@ -13,12 +13,12 @@
 	*/
 
 	var defaultKey		= 'I7t7etej0JxF3v5h7tTkTT6Ts3Yx3Jk9lSiB0X04zfjzUy85', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [802239190], // Comma separated array of Xively Feed ID numbers
+		defaultFeeds	= [1842162545], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'SmartOrbis App', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= 'FF0000', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		dataColor		= '#4682B4', // CSS HEX value of color to represent data (omit leading #)
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
@@ -148,7 +148,7 @@
 										element: document.querySelector('#graph-' + feedId + '-' + datastream.id),
 										width: 600,
 										height: 200,
-										renderer: 'bar',
+										renderer: 'line',
 										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										padding: {
